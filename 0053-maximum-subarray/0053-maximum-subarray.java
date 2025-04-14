84ee983ddc754b3b9fa75b3a68fe354b31bb8637
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-13 20:11:51 -0400

    Time: 1 ms (99.54%), Space: 58.1 MB (8.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -30,17 +30,29 @@ class Solution {
     //     return Math.max(maxLeftRight,leftSum+rightSum);
 
     //Kadane
-    int currSum=nums[0];
-    int maxSum=nums[0];
+    // int currSum=nums[0];
+    // int maxSum=nums[0];
+    // for(int i=1;i<nums.length;i++){
+    //     if(currSum<0 && nums[i]>currSum){
+    //         currSum=nums[i];
+    //     }else{
+    //         currSum+=nums[i];
+    //     }
+    //     maxSum=Math.max(currSum,maxSum);
+    // }
+    // return maxSum;
+
+    //DP
+    int[] dp=new int[nums.length];
+    int max=nums[0];
+    dp[0]=nums[0];
     for(int i=1;i<nums.length;i++){
-        if(currSum<0 && nums[i]>currSum){
-            currSum=nums[i];
-        }else{
-            currSum+=nums[i];
-        }
-        maxSum=Math.max(currSum,maxSum);
+      dp[i]=Math.max(dp[i-1]+nums[i],nums[i]);
+      if(dp[i]>max){
+        max=dp[i];
+      }  
     }
-    return maxSum;
+    return max;
 
     }
 }
