2a21a647ca34943bbfdc5fbf865ec25240c1f450
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-15 01:06:25 -0400

    Time: 2 ms (84.8%), Space: 46.2 MB (32.24%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,17 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0; // Pointer for where the next non-zero should go
-        int j = 0; // Pointer for traversing the array
-
-        while (j < nums.length) {
-            if (nums[j] != 0) {
-                // Swap nums[i] and nums[j]
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
+        int i=0;
+        int j=0;
+        while(j<nums.length){
+            if(nums[j]!=0){
+                int temp=nums[i];
+                nums[i]=nums[j];
+                nums[j]=temp;
                 i++;
             }
-            j++;
+                j++;
+        
         }
     }
 }
