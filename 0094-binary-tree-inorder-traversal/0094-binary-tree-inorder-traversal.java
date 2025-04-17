ec3cd34dc9e44e77678c859f01cfe2fb8e9615ce
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-17 02:50:31 -0400

    Time: 0 ms (100%), Space: 42 MB (8.41%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -13,19 +13,40 @@
  *     }
  * }
  */
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-       List<Integer> res=new ArrayList<>();
-       helper(root,res);
-       return res; 
-    }
-    public void helper(TreeNode root,List<Integer> res){
-        if(root!=null){
-            helper(root.left,res);
-            res.add(root.val);
-            helper(root.right,res);
-        }
-    }
-}
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//        List<Integer> res=new ArrayList<>();
+//        helper(root,res);
+//        return res; 
+//     }
+//     public void helper(TreeNode root,List<Integer> res){
+//         if(root!=null){
+//             helper(root.left,res);
+//             res.add(root.val);
+//             helper(root.right,res);
+//         }
+//     }
+// }
 
 //Dfs InOrder- Go to Left node->Add Root Node->Go to Right node
+
+//Iterative stack 
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root){
+        List<Integer> res=new ArrayList<>();
+        Stack<TreeNode> stack=new Stack<>();
+
+        while(root!=null || !stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root=root.left;
+            }
+
+            root=stack.pop();
+            res.add(root.val);
+            root=root.right;
+        }
+
+        return res;
+    }
+}
