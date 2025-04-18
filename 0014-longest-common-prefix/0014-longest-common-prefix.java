209ea79385a48721acd269c8868afcfebca7a638
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-18 00:41:21 -0400

    Time: 0 ms (100%), Space: 41.7 MB (41.63%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,19 +1,19 @@
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
-        String s1=strs[0];
-        String s2=strs[strs.length-1];
-        int idx=0;
-        while(idx<s1.length() && idx<s2.length()){
-            if(s1.charAt(idx)==s2.charAt(idx)){
-                idx++;
-            }else{
-                break;
-            }
-        }
-        return s1.substring(0,idx);
-    }
-}
+// class Solution {
+//     public String longestCommonPrefix(String[] strs) {
+//         Arrays.sort(strs);
+//         String s1=strs[0];
+//         String s2=strs[strs.length-1];
+//         int idx=0;
+//         while(idx<s1.length() && idx<s2.length()){
+//             if(s1.charAt(idx)==s2.charAt(idx)){
+//                 idx++;
+//             }else{
+//                 break;
+//             }
+//         }
+//         return s1.substring(0,idx);
+//     }
+// }
 
 // \U0001f914 But why check s2.length()?
 // Even though s1 is the first string in the sorted array and s2 is the last string, s2 can be shorter than s1.
@@ -58,3 +58,16 @@ class Solution {
 // while(idx < s1.length() && idx < s2.length())
 // Because it ensures you're not reading past the end of either string while comparing characters.
 
+//-------------------------------------
+class Solution{
+    public String longestCommonPrefix(String[] strs) {
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(!strs[i].startsWith(prefix)){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
