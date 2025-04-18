8df72ab800cdcd0dc0d53af4711408e88eeb6303
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-17 23:25:18 -0400

    Time: 0 ms (100%), Space: 44.6 MB (50.27%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+//A map is a BST, unordered_map is a hast table.
+// insertions and lookup have an O(logn) time complexity in a map and O(1) in an unordered_map
+
