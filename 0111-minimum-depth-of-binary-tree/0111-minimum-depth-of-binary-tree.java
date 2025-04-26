24386d8f7419cb35ad1322f5acf59216219b9df7
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-26 01:59:01 -0400

    Time: 5 ms (48.3%), Space: 63.4 MB (46.73%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -13,32 +13,50 @@
  *     }
  * }
  */
+// class Solution {
+//     int depth = 0;
+
+//     public int minDepth(TreeNode root) {
+//         if (root != null) {
+//             return dfs(root, 1); // Start at depth 1
+//         }
+//         return 0;
+//     }
+
+//     public int dfs(TreeNode node, int depth) {
+//         // If it's a leaf node
+//         if (node.left == null && node.right == null) {
+//             return depth;
+//         }
+
+//         int leftDepth = Integer.MAX_VALUE;
+//         int rightDepth = Integer.MAX_VALUE;
+
+//         if (node.left != null) {
+//             leftDepth = dfs(node.left, depth + 1);
+//         }
+//         if (node.right != null) {
+//             rightDepth = dfs(node.right, depth + 1);
+//         }
+
+//         return Math.min(leftDepth, rightDepth);
+//     }
+// }
+//------------------------------------------------
+//dfs
 class Solution {
-    int depth = 0;
-
     public int minDepth(TreeNode root) {
-        if (root != null) {
-            return dfs(root, 1); // Start at depth 1
-        }
-        return 0;
+        if(root==null) return 0;
+        return dfs(root);
     }
+    private int dfs(TreeNode node){
+        if(node==null) return Integer.MAX_VALUE;
 
-    public int dfs(TreeNode node, int depth) {
-        // If it's a leaf node
-        if (node.left == null && node.right == null) {
-            return depth;
-        }
+        if(node.left==null && node.right==null) return 1;
 
-        int leftDepth = Integer.MAX_VALUE;
-        int rightDepth = Integer.MAX_VALUE;
+        int left=dfs(node.left);
+        int right=dfs(node.right);
 
-        if (node.left != null) {
-            leftDepth = dfs(node.left, depth + 1);
-        }
-        if (node.right != null) {
-            rightDepth = dfs(node.right, depth + 1);
-        }
-
-        return Math.min(leftDepth, rightDepth);
+        return 1+Math.min(left,right);
     }
 }
