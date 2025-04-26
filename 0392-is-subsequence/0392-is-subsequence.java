e524671e9a5122bb2d0be0f94ba30aef4c421aeb
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-04-25 23:06:56 -0400

    Time: 5 ms (3.69%), Space: 41.9 MB (15.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        boolean[][] dp = new boolean[m + 1][n + 1];
+
+        // An empty string s is always a subsequence of any t
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = true;
+        }
+
+        // Fill the dp table
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[m][n];
+    }
+}
