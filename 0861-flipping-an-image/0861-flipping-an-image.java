6149f9be10d7282327cd1c97c6379ad94236f056
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-07 17:11:35 -0400

    Time: 0 ms (100%), Space: 44.7 MB (40.95%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.java
+++ 0861-flipping-an-image/0861-flipping-an-image.java
@@ -11,3 +11,52 @@ class Solution {
         return image;
     }
 }
+//If col = 5, then:
+
+// java
+// Copy
+// Edit
+// (col + 1) / 2 = (5 + 1) / 2 = 6 / 2 = 3
+// You get 3 iterations, which covers:
+
+// index 0 ↔ 4
+
+// index 1 ↔ 3
+
+// index 2 ↔ itself (center element)
+
+// The center element (at index 2) doesn’t move during a flip, but it still needs to be inverted, which is why we include it in the loop.
+
+//-----------------------------------------
+
+// class Solution {
+//     public int[][] flipAndInvertImage(int[][] image) {
+        
+//         for(int[] arr : image){
+//             reverse(arr);
+//             invert(arr);
+//         }
+//         return image;
+//     }
+
+//     private void reverse(int[] arr) {
+//         int start = 0;
+//         int end   = arr.length - 1;
+
+//        while(start < end) {
+//             int temp  = arr[start];
+//             arr[start] = arr[end];
+//             arr[end]  = temp;
+
+//             start += 1;
+//             end   -= 1;
+//        }  
+//     }
+
+//     private void invert(int[] arr) {
+//         for(int i = 0; i < arr.length; i++) {
+//             if(arr[i] == 0) arr[i] = 1;
+//             else arr[i] = 0;
+//         }
+//     }
+// }
