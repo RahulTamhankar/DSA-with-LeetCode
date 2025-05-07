61ceca7b5377e4c834038641d592c8d090fcda70
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-07 02:06:15 -0400

    Time: 0 ms (100%), Space: 40.9 MB (70.53%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -26,5 +26,6 @@ class Solution {
     public int arrangeCoins(int n) {
        return (int)(Math.sqrt(2 * (long)n + 0.25) - 0.5);
     }
+    // integer can overflow hence long. Then entire result to be converted to int
 
 }
