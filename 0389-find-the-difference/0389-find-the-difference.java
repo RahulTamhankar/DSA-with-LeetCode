7b233bb8c58ac0b8472a467625a94f8e7d5311f4
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-08 00:16:52 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,3 +1,47 @@
+//Approach-1
+//T.C : Linear
+//S.C : Extra space map
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        Map<Character, Integer> mp = new HashMap<>();
+        
+        for (char ch : s.toCharArray()) {
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        }
+        
+        for (char ch : t.toCharArray()) {
+            mp.put(ch, mp.getOrDefault(ch, 0) - 1);
+            
+            if (mp.get(ch) < 0)
+                return ch;
+        }
+        
+        return 'a'; // send any random character; it will never reach here
+    }
+}
+
+
+//Approach-2 (Using Sum Difference)
+//T.C : Lines
+//S.C : constant
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int sum = 0;
+        
+        for (char ch : t.toCharArray())
+            sum += ch;
+        
+        for (char ch : s.toCharArray())
+            sum -= ch;
+        
+        return (char) sum;
+    }
+}
+
+
+//Approach-3 (Using XOR property)
+//T.C : Linear
+//S.C : constant
 class Solution {
     public char findTheDifference(String s, String t) {
         int xor=0;
