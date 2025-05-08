089be09eb9b0d74ec9d5be0ce5bf9d6432a46ec0
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-08 00:16:54 -0400

    Time: 1 ms (99.3%), Space: 41.7 MB (90.07%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,42 +1,42 @@
-//Approach-1
-//T.C : Linear
-//S.C : Extra space map
-public class Solution {
-    public char findTheDifference(String s, String t) {
-        Map<Character, Integer> mp = new HashMap<>();
+// //Approach-1
+// //T.C : Linear
+// //S.C : Extra space map
+// public class Solution {
+//     public char findTheDifference(String s, String t) {
+//         Map<Character, Integer> mp = new HashMap<>();
         
-        for (char ch : s.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
-        }
+//         for (char ch : s.toCharArray()) {
+//             mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+//         }
         
-        for (char ch : t.toCharArray()) {
-            mp.put(ch, mp.getOrDefault(ch, 0) - 1);
+//         for (char ch : t.toCharArray()) {
+//             mp.put(ch, mp.getOrDefault(ch, 0) - 1);
             
-            if (mp.get(ch) < 0)
-                return ch;
-        }
+//             if (mp.get(ch) < 0)
+//                 return ch;
+//         }
         
-        return 'a'; // send any random character; it will never reach here
-    }
-}
+//         return 'a'; // send any random character; it will never reach here
+//     }
+// }
 
 
-//Approach-2 (Using Sum Difference)
-//T.C : Lines
-//S.C : constant
-public class Solution {
-    public char findTheDifference(String s, String t) {
-        int sum = 0;
+// //Approach-2 (Using Sum Difference)
+// //T.C : Lines
+// //S.C : constant
+// public class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int sum = 0;
         
-        for (char ch : t.toCharArray())
-            sum += ch;
+//         for (char ch : t.toCharArray())
+//             sum += ch;
         
-        for (char ch : s.toCharArray())
-            sum -= ch;
+//         for (char ch : s.toCharArray())
+//             sum -= ch;
         
-        return (char) sum;
-    }
-}
+//         return (char) sum;
+//     }
+// }
 
 
 //Approach-3 (Using XOR property)
