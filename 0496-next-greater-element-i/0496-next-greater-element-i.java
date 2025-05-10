80bf66a17279fb57b0fef0f9a04bbd6519da05cc
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-10 16:44:34 -0400

    Time: 3 ms (90.33%), Space: 43.9 MB (48.97%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,38 +1,25 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        
-    if (nums2.length == 0 || nums1.length == 0)
-      return new int[0];
+	int[] ans = new int[nums1.length];
 
-    Map<Integer, Integer> numberNGE = new HashMap<>();
-    Stack<Integer> numStack = new Stack<>();
+	Stack<Integer> stack = new Stack<>();
+	HashMap<Integer, Integer> map = new HashMap<>();
 
-    numStack.push(nums2[nums2.length - 1]);
-    numberNGE.put(nums2[nums2.length - 1], -1);
+	// find out all the next greater elements in nums2 array
+	for(int num: nums2) {
+		// if num is greater than top elements in stack then it is the next greater element in nums2
+		while(!stack.isEmpty() && num > stack.peek()) {
+			map.put(stack.pop(), num);
+		}
+		// then add num to stack
+		stack.add(num);
+	}
 
-    for (int i = nums2.length - 2; i >= 0; i--) {
+	int i = 0;
+	for(int num : nums1) {
+		ans[i++] = map.getOrDefault(num, -1);
+	}
 
-      if (nums2[i] < numStack.peek()) {
-        numberNGE.put(nums2[i], numStack.peek());
-        numStack.push(nums2[i]);
-        continue;
-      }
-
-      while (!numStack.isEmpty() && numStack.peek() < nums2[i])
-        numStack.pop();
-
-      if (numStack.isEmpty()) {
-        numStack.push(nums2[i]);
-        numberNGE.put(nums2[i], -1);
-      } else {
-        numberNGE.put(nums2[i], numStack.peek());
-        numStack.push(nums2[i]);
-      }
-    }
-
-    for (int i = 0; i < nums1.length; i++)
-      nums1[i] = numberNGE.get(nums1[i]);
-
-    return nums1;
-  }
+	return ans;
+}
 }
