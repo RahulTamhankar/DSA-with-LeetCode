1c14c26cf7cf4e0988980d4993f54c0afc7bd520
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-14 01:53:28 -0400

    Time: 1 ms (100%), Space: 44.9 MB (13.33%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,26 +1,73 @@
+// /**
+//  * Definition for singly-linked list.
+//  * public class ListNode {
+//  *     int val;
+//  *     ListNode next;
+//  *     ListNode() {}
+//  *     ListNode(int val) { this.val = val; }
+//  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  * }
+//  */
+// class Solution {
+//     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//         ListNode l3=new ListNode(0);
+//         int carry=0;
+//         ListNode head=l3;
+
+//         while(l1 && l2){
+//             int value= l1.val+l2.val+carry;
+//             carry=value/10;
+//             l3=l3.next;
+//             l1=l1.next;
+//             l2=l2.next;
+//         }
+
+//         while(l2){
+//             int value= l2.val+carry;
+//             carry=value/10;
+//             l3=l3.next;
+//             l2=l2.next;
+//         }
+//         while(l1){
+//             int value= l1.val+carry;
+//             carry=value/10;
+//             l3=l3.next;
+//             l1=l1.next;
+//         }
+//         if(carry){
+//             l3.next=new ListNode(carry);
+//         }
+//         return head.next;
+        
+//     }
+// }
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummy = new ListNode(0); // creating an dummy list
-        ListNode curr = dummy; // intialising an pointer
-        int carry = 0; // intialising our carry with 0 intiall
-        // while loop will run, until l1 OR l2 not reaches null OR if they both reaches null. But our carry has some value in it. 
-		// We will add that as well into our list
-        while(l1 != null || l2 != null || carry == 1){
-            int sum = 0; // intialising our sum
-            if(l1 != null){ // adding l1 to our sum & moving l1
-                sum += l1.val;
-                l1 = l1.next;
-            }
-            if(l2 != null){ // adding l2 to our sum & moving l2
-                sum += l2.val;
-                l2 = l2.next;
-            }
-            sum += carry; // if we have carry then add it into our sum
-            carry = sum/10; // if we get carry, then divide it by 10 to get the carry
-            ListNode node = new ListNode(sum % 10); // the value we'll get by moduloing it, will become as new node so. add it to our list
-            curr.next = node; // curr will point to that new node if we get
-            curr = curr.next; // update the current every time
+        ListNode l3 = new ListNode(0);  // dummy head
+        ListNode head = l3;             // to return result later
+        int carry = 0;
+
+        // Process both lists as long as either has nodes
+        while (l1 != null || l2 != null) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            l3.next = new ListNode(sum % 10);
+            l3 = l3.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
         }
-        return dummy.next; // return dummy.next bcz, we don't want the value we have consider in it intially!!
+
+        // If there's a carry left after all additions
+        if (carry != 0) {
+            l3.next = new ListNode(carry);
+        }
+
+        return head.next;
     }
 }
