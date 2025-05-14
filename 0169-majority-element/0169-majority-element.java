545b6e50a177285a1ce6e576d7474fb7e739f37b
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-14 00:45:58 -0400

    Time: 1 ms (99.85%), Space: 53.2 MB (49.48%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,3 +15,16 @@ class Solution {
         return candidate;
     }
 }
+
+//Boyer-Moore Majority Vote Algorithm.
+// Input: [2,2,1,1,1,2,2]
+// Output: 2
+// | n | candidate | count |
+// | - | --------- | ----- |
+// | 2 | 2         | 1     |
+// | 2 | 2         | 2     |
+// | 1 | 2         | 1     |
+// | 1 | 2         | 0     |
+// | 1 | 1         | 1     |
+// | 2 | 1         | 0     |
+// | 2 | 2         | 1     |
