437747cc4e98ee3ea74cb85864cf73a290d8e975
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-14 21:01:56 -0400

    Time: 6 ms (81%), Space: 56.4 MB (80.42%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -16,3 +16,15 @@ class Solution {
         return ans;
     }
 }
+
+
+// Why -1 needed in int index = Math.abs(nums[i]) - 1 ?
+// The array has 0-based indexing, but the values in nums are 1-based.
+// Say nums = [4,3,2,7,8,2,3,1]
+
+// The values go from 1 to n = 8
+
+// But array indices go from 0 to 7
+
+// If nums[i] = 4, that means number 4 exists in the array.
+// To mark it as visited, we need to make nums[3] (i.e., index 4-1) negative.
