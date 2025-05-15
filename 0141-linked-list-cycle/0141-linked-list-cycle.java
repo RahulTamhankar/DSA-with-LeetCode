dde3a0ca9b9729f149665e6536c6a0219cc0efb3
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-14 20:17:21 -0400

    Time: 0 ms (100%), Space: 44.9 MB (18.41%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -24,6 +24,14 @@ public class Solution {
     }
 }
 
+
+// We did this in finding middle element fast == null || fast.next == null,
+// But In the cycle detection version:
+
+// You’re not interested in the middle.
+
+// You're checking whether slow == fast, which would only happen if there's a loop (they'll keep chasing each other in the cycle until they meet).
+
 //A map is a BST, unordered_map is a hast table.
 // insertions and lookup have an O(logn) time complexity in a map and O(1) in an unordered_map
 
