7a5722d610b60417124527c00ece3119c92d65d2
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-18 14:13:51 -0400

    Time: 1 ms (99.85%), Space: 53.4 MB (21.62%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int majorityElement(int[] nums) {
-        int count=0;
-        int candidate=0;
-        for(int n : nums){
-            if(count==0){
-                candidate=n;
-            }
-            if(n==candidate){
-                count++;
-            }else{
-                count--;
-            }
-        }
-        return candidate;
-    }
-}
+// class Solution {
+//     public int majorityElement(int[] nums) {
+//         int count=0;
+//         int candidate=0;
+//         for(int n : nums){
+//             if(count==0){
+//                 candidate=n;
+//             }
+//             if(n==candidate){
+//                 count++;
+//             }else{
+//                 count--;
+//             }
+//         }
+//         return candidate;
+//     }
+// }
 
 //Boyer-Moore Majority Vote Algorithm.
 // Input: [2,2,1,1,1,2,2]
@@ -28,3 +28,23 @@ class Solution {
 // | 1 | 1         | 1     |
 // | 2 | 1         | 0     |
 // | 2 | 2         | 1     |
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0]; // Fixed typo from 'candiddate' and 'nus'
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = nums[i]; // pick a new candidate
+                    count = 1;
+                }
+            }
+        }
+
+        return candidate;
+    }
+}
