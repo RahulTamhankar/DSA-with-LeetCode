0cef1404290299bd6d287169583fdd51c96a5e72
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-18 21:11:13 -0400

    Time: 0 ms (100%), Space: 44.6 MB (84%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -21,7 +21,7 @@ class Solution {
     }
 
     private int helper(TreeNode node){
-        if(node==null) return 0;
+        if(node==null) return 0; 
 
         int leftDepth=helper(node.left);
         int rightDepth=helper(node.right);
@@ -39,4 +39,28 @@ class Solution {
 // It’s used by the parent node to calculate its own depth.
 
 // It's not the diameter — just the depth of the longest path down from this node to a leaf.
+
+
+//Mistake done 
+// public void helper(TreeNode node){
+// 	if(node.left==null || node.right==null) return null;
+
+//      1
+//        / \
+//       2   3
+//      / \
+//     4   5
+// Longest path: [4 → 2 → 1 → 3]
+
+// Number of edges = 3 → that's the diameter
+
+// Let’s Trace at Node 1:
+// leftDepth = depth of node 2 = 2 (2 → 4 or 5)
+
+// rightDepth = depth of node 3 = 1
+
+// currentDiameter = 2 + 1 = 3
+
+// ✅ Yes, that’s correct — and it matches the actual diameter of the tree.
+
 
