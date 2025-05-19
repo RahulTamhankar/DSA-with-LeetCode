c8e78f79d10d40cd215e7ee38e3cb9ac319135fe
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-19 00:33:31 -0400

    Time: 0 ms (100%), Space: 41.2 MB (35.26%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,65 @@
+//Rec +Memo=TopDown
+//Bottom Up
+
+//Why DP ? Since robber has many choices,say he can got to house with 1 or not go... ||ly can got to 3 or not go
+//1)We have options to chose
+//2)Maximum or optimal is asked
+//3)Overlapping subproblems eg. (3,1) (2,3,1)->(3,1)
+
+
+//Approach-1 (Recur + Memo)
+// class Solution {
+// public:
+//     int t[101];
+//     int solve(vector<int>& nums, int i, int& n) {
+//         if(i >= n)
+//             return 0;
+        
+//         if(t[i] != -1)
+//             return t[i];
+        
+//         int take = nums[i] + solve(nums, i+2, n); //steals ith house and moves to i+2 (because we can't steal adjacent)
+//         int skip = solve(nums, i+1, n); //skips this house, now we can move to adjacent next house
+        
+//         return t[i]=max(take, skip);
+//     }
+    
+//     int rob(vector<int>& nums) {
+//         int n = nums.size();
+//         memset(t, -1, sizeof(t));
+//         return solve(nums, 0, n);
+//     }
+// };
+
+            /*
+                SKIP  : If we skip this house,  then we have money till previous house  =  t[i-1]
+                STEAL : If we steal this house, then we can't take prev profit, we can take till (i-2)th house profit = t[i-2]
+            */
+
+
+
+class Solution {
+    public int rob(int[] nums) {
+        // Bottom-Up DP (Tabulation)
+        // Time: O(n)
+        // Space: O(n)
+
+        int n = nums.length;
+        if (n == 1) {
+            return nums[0];
+        }
+        if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[n - 1];
+    }
+}
