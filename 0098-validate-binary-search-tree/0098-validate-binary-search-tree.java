9e93b3829bcd1f9945bea45dd27e619cb7c1b5f8
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-25 14:46:01 -0400

    Time: 1 ms (19.77%), Space: 43.3 MB (79.33%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -12,7 +12,11 @@
  *         this.right = right;
  *     }
  * }         
- */                  
+ */           
+//We'll do in-order traversal using a stack (left → root → right).       
+//pre will store the previously visited node (we compare it with the current).
+
+
 class Solution {      
     public boolean isValidBST(TreeNode root) {
         if(root==null) return true;
@@ -24,6 +28,7 @@ class Solution {
                 stack.push(root);
                 root=root.left;
             }
+            //go as left as possible and then visit at the top of stack(current root)
             root=stack.pop();
             if(pre!=null && root.val<=pre.val) return false;
             pre=root;
@@ -32,3 +37,27 @@ class Solution {
         return true;
     }                
 }                    
+
+// class Solution {
+//     TreeNode prev;
+        
+//     public boolean isValidBST(TreeNode root) {
+//         if (root == null)
+//             return true;
+        
+//         if(!isValidBST(root.left))
+//             return false;
+        
+//         if (prev != null && prev.val >= root.val)
+//             return false;
+        
+//         prev = root;
+        
+//         if (!isValidBST(root.right))
+//             return false;
+        
+//         return true;
+        
+        
+//     }
+// }
