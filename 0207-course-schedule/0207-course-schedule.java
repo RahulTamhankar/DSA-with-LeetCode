be1748c2df070c8418f48dc303b4bf677125c4fa
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-26 17:10:39 -0400

    Time: 6 ms (74.28%), Space: 45.6 MB (42.38%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,46 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int count = 0;
+        Queue<Integer> q = new LinkedList<>();
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            adj.add(new ArrayList<>());
+        }
+
+        // Build graph and fill indegrees
+        for (int[] pre : prerequisites) {
+            adj.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        boolean[] visited = new boolean[numCourses];
+
+        // Add all nodes with indegree 0 to queue
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+                visited[i] = true;
+                count++;
+            }
+        }
+
+        // BFS (Kahn's Algorithm)
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    indegree[neighbor]--;
+                    if (indegree[neighbor] == 0) {
+                        q.offer(neighbor);
+                        visited[neighbor] = true;
+                        count++;
+                    }
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
+}
