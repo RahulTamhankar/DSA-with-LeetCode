cbe058c6718276c8ae17ed7f96781ea1b5fc0ab8
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-25 23:39:52 -0400

    Time: 0 ms (100%), Space: 40.6 MB (86.93%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -14,7 +14,30 @@
 //         }
 //     }
 // }
+//Why only 1 and 7 are happy single-digit numbers?
+// Because:
 
+// 1 is trivially happy
+
+// 7 transforms into 1 after a few steps:
+
+// 7² = 49
+
+// 4² + 9² = 97
+
+// 9² + 7² = 130
+
+// 1² + 3² + 0² = 10
+
+// 1² + 0² = 1 ✅
+
+
+
+//----------------------------------------------------
+//If there is a cycle, no is not happy
+// The key idea:
+// If there is a cycle, fast and slow will eventually meet.
+// If the number is happy, fast will eventually become 1.
 //Floyd cycle
 class Solution {
     public boolean isHappy(int n){
@@ -38,3 +61,18 @@ class Solution {
         return sum;
     }
 }
+
+//\U0001f504 If the number is not happy
+// Example: n = 2
+
+// You will end up in a cycle:
+
+// Copy
+// Edit
+// 2 → 4 → 16 → 37 → 58 → 89 → 145 → 42 → 20 → 4 → ...
+// slow and fast will eventually meet at the same number (!= 1)
+
+// So: while (fast != 1 && slow != fast) breaks when slow == fast
+
+// Return false
+
