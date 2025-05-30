50f3319412aab7994b371bba5a63e3a3f5c6116d
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-29 21:54:04 -0400

    Time: 0 ms (100%), Space: 40.6 MB (48.68%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -17,3 +17,24 @@ class Solution {
         return dp[m - 1][n - 1];
     }
 }
+
+
+// The number of unique paths can be seen as the number of ways to choose m−1 downs and n−1 rights, regardless of the order. In combinatorial terms, this is equivalent to 
+// ( m−1
+// m+n−2).
+// Algorithm
+// Use the Combinatorial Formula:
+// ( m−1
+// m+n−2) or 
+// ( n−1
+// m+n−2) to calculate the number of unique paths.
+
+// public class Solution {
+//     public int uniquePaths(int m, int n) {
+//         long ans = 1;
+//         for (int i = 1; i <= m - 1; i++) {
+//             ans = ans * (n - 1 + i) / i;
+//         }
+//         return (int)ans;
+//     }
+// }
