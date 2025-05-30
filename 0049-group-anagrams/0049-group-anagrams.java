4e85bca1bd0d9c56dba7c5c3cbc6e61448c195c0
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-29 20:08:00 -0400

    Time: 15 ms (29.3%), Space: 48 MB (45.45%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,3 +1,86 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+    // Check for empty inputs
+    if (strs == null || strs.length == 0)
+      return new ArrayList<>();
+
+    Map<String, List<String>> frequencyStringsMap = new HashMap<>();
+    for (String str : strs) {
+
+      String frequencyString = getFrequencyString(str);
+
+      // If the frequency string is present, add the string to the list
+      if (frequencyStringsMap.containsKey(frequencyString)) {
+        frequencyStringsMap.get(frequencyString).add(str);
+      }
+      else {
+        // else create a new list
+        List<String> strList = new ArrayList<>();
+        strList.add(str);
+        frequencyStringsMap.put(frequencyString, strList);
+      }
+    }
+
+    return new ArrayList<>(frequencyStringsMap.values());
+  }
+
+  private String getFrequencyString(String str) {
+
+    // Frequency buckets
+    int[] freq = new int[26];
+
+    // Iterate over each character
+    for (char c : str.toCharArray()) {
+      freq[c - 'a']++;
+    }
+
+    // Start creating the frequency string
+    StringBuilder frequencyString = new StringBuilder("");
+    char c = 'a';
+    for (int i : freq) {
+      frequencyString.append(c);
+      frequencyString.append(i);
+      c++;
+    }
+
+    return frequencyString.toString();
+  }
+}
+
+
+
+
+
+
+
+
+
+// public List<List<String>> groupAnagramsCategorizeBySorting(String[] strs) {
+
+//     if (strs == null || strs.length == 0)
+//       return new ArrayList<>();
+
+//     Map<String, List<String>> stringAnagramsMap = new HashMap<>();
+//     for (String s : strs) {
+//       char[] arr = s.toCharArray();
+//       Arrays.sort(arr);
+//       String key = String.valueOf(arr);
+
+//       if (!stringAnagramsMap.containsKey(key))
+//         stringAnagramsMap.put(key, new ArrayList<>());
+
+//       stringAnagramsMap.get(key).add(s);
+//     }
+
+//     List<List<String>> resultList = new ArrayList<>();
+//     for (Map.Entry<String, List<String>> stringAnagrams : stringAnagramsMap.entrySet()) {
+//       resultList.add(stringAnagrams.getValue());
+//     }
+//     return resultList;
+//   }
+
+//---------------CodewMie
 // //Approach-1 (Using Sorting)
 // //T.C : O(n*klog(k))  (n = size of input, k = maximum length of a string in the input vector)
 // //S.C : O(n*k)
@@ -22,41 +105,41 @@
 // }
 
 
-//Approach-2
-//T.C : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
-//S.C : O(n*k)
-class Solution {
-    private String generate(String s) {
-        int[] count = new int[26];
+// //Approach-2
+// //T.C : O(n*k)  (n = size of input, k = maximum length of a string in the input vector)
+// //S.C : O(n*k)
+// class Solution {
+//     private String generate(String s) {
+//         int[] count = new int[26];
 
-        for (char ch : s.toCharArray()) {
-            count[ch - 'a']++;
-        }
+//         for (char ch : s.toCharArray()) {
+//             count[ch - 'a']++;
+//         }
 
-        StringBuilder newS = new StringBuilder();
+//         StringBuilder newS = new StringBuilder();
 
-        for (int i = 0; i < 26; i++) {
-            if (count[i] > 0) {
-                newS.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
-            }
-        }
+//         for (int i = 0; i < 26; i++) {
+//             if (count[i] > 0) {
+//                 newS.append(String.valueOf((char) (i + 'a')).repeat(count[i]));
+//             }
+//         }
 
-        return newS.toString();
-    }
+//         return newS.toString();
+//     }
 
-    public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> map = new HashMap<>();
+//     public List<List<String>> groupAnagrams(String[] strs) {
+//         Map<String, List<String>> map = new HashMap<>();
 
-        for (String s : strs) {
-            String newS = generate(s);
+//         for (String s : strs) {
+//             String newS = generate(s);
 
-            if (!map.containsKey(newS)) {
-                map.put(newS, new ArrayList<>());
-            }
+//             if (!map.containsKey(newS)) {
+//                 map.put(newS, new ArrayList<>());
+//             }
 
-            map.get(newS).add(s);
-        }
+//             map.get(newS).add(s);
+//         }
 
-        return new ArrayList<>(map.values());
-    }
-}
+//         return new ArrayList<>(map.values());
+//     }
+// }
