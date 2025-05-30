63e4af536c1a2ca46827dc16c989dc5490d83a7d
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-29 23:03:10 -0400

    Time: 0 ms (100%), Space: 42 MB (39.72%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,48 +1,54 @@
-class Solution {
-    private int m,n;
+public class Solution {
+    private int m, n;
+
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        m=obstacleGrid.length;
-        n=obstacleGrid[0].length;
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+
         return solve(obstacleGrid);
     }
 
-    private int solve(int[][] obstacleGrid){
-        int[][] t=new int[m][n];
+    private int solve(int[][] obstacleGrid) {
+        int[][] t = new int[m][n];
 
-        if(obstacleGrid[0][0]==1)
+        if (obstacleGrid[0][0] == 1)
             return 0;
 
-        for(int col=0;col<n;col++){
-            if(col>0 && obstacleGrid[0][col-1]==1){
-                t[0][col]=0;
-                obstacleGrid[0][col]=1;
-            }else if(obstacleGrid[0][col]==1){
-                t[0][col]=0;
-            }else{
-                t[0][col]=1;
+        // Fill first row
+        for (int col = 0; col < n; col++) {
+            if (col > 0 && obstacleGrid[0][col - 1] == 1) {
+                t[0][col] = 0;
+                obstacleGrid[0][col] = 1; // Block further cells in the row
+            } else if (obstacleGrid[0][col] == 1) {
+                t[0][col] = 0;
+            } else {
+                t[0][col] = 1;
             }
         }
 
-        for(int row=0;row<m;row++){
-            if(row>0 && obstacleGrid[row-1][0]==1){
-                t[row][0]=0;
-                obstacleGrid[row][0]=1;
-            }else if(obstacleGrid[row][0]==1){
-                t[row][0]=0;
-            }else{
-                t[row][0]=1;
+        // Fill first column
+        for (int row = 0; row < m; row++) {
+            if (row > 0 && obstacleGrid[row - 1][0] == 1) {
+                t[row][0] = 0;
+                obstacleGrid[row][0] = 1; // Block further cells in the column
+            } else if (obstacleGrid[row][0] == 1) {
+                t[row][0] = 0;
+            } else {
+                t[row][0] = 1;
             }
         }
 
-        for(int i=1;i<m;i++){
-            for(int j=1;j<n;j++){
-                if(obstacleGrid[i][j]==1){
-                    t[i][j]=0;
-                }else{
-                    t[i][j]=t[i-1][j]+t[i][j-1];
+        // Fill rest of the table
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    t[i][j] = 0;
+                } else {
+                    t[i][j] = t[i - 1][j] + t[i][j - 1]; //t[i-1][j] is from top cell and t[i][j - 1] is from left cell
                 }
             }
         }
-        return t[m-1][n-1];
+
+        return t[m - 1][n - 1];
     }
 }
