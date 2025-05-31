8443e2f2120ca7baa82049e8ed25740639e117be
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-05-31 14:14:54 -0400

    Time: 0 ms (100%), Space: 42.3 MB (59.82%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,26 +1,19 @@
 class Solution {
     public void rotate(int[][] matrix) {
-         int n = matrix.length;
+        int n=matrix.length;
 
-    for (int i = 0; i < (n + 1) / 2; i ++) {
+        for(int i=0;i<(n+1)/2;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[n-1-j][i];
 
-      for (int j = 0; j < n / 2; j++) {
-        // Start 4 way swaps
-        // temp = bottom left
-        int temp = matrix[n - 1 - j][i];
+                matrix[n-1-j][i]=matrix[n-1-i][n-j-1];
 
-        // bottom left = bottom right
-        matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n-1-i][n-j-1]=matrix[j][n-1-i];
 
-        // bottom right = top right
-        matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 -i];
+                matrix[j][n-1-i]=matrix[i][j];
 
-        // top right = top left
-        matrix[j][n - 1 - i] = matrix[i][j];
-
-        // top left = temp
-        matrix[i][j] = temp;
-      }
-    }
+                matrix[i][j]=temp;
+            }
+        }
     }
 }
