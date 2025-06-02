bf61ff80e125367dc32670b66c2aee4e8df3a580
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-01 22:06:56 -0400

    Time: 1 ms (99.82%), Space: 46 MB (66.12%) - LeetHub

--- 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.java
+++ 0930-all-possible-full-binary-trees/0930-all-possible-full-binary-trees.java
@@ -13,30 +13,60 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<TreeNode> allPossibleFBT(int n) {
+//         if (n % 2 == 0) return new ArrayList<>(); // Full BTs only have odd number of nodes
+
+//         List<TreeNode> ans = new ArrayList<>();
+//         if (n == 1) {
+//             ans.add(new TreeNode(0));
+//             return ans;
+//         }
+
+//         for (int i = 1; i < n; i += 2) {
+//             List<TreeNode> leftTrees = allPossibleFBT(i);
+//             List<TreeNode> rightTrees = allPossibleFBT(n - 1 - i);
+
+//             for (TreeNode left : leftTrees) {
+//                 for (TreeNode right : rightTrees) {
+//                     TreeNode root = new TreeNode(0);
+//                     root.left = left;
+//                     root.right = right;
+//                     ans.add(root);
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
+
+//Optimal
 class Solution {
+    Map<Integer,List<TreeNode>> dp=new HashMap<>();
     public List<TreeNode> allPossibleFBT(int n) {
-        if (n % 2 == 0) return new ArrayList<>(); // Full BTs only have odd number of nodes
+       if(n%2==0) return new ArrayList<>();
 
-        List<TreeNode> ans = new ArrayList<>();
-        if (n == 1) {
-            ans.add(new TreeNode(0));
-            return ans;
-        }
+       if(n==1) return Arrays.asList(new TreeNode(0));
 
-        for (int i = 1; i < n; i += 2) {
-            List<TreeNode> leftTrees = allPossibleFBT(i);
-            List<TreeNode> rightTrees = allPossibleFBT(n - 1 - i);
+       if(dp.containsKey(n)) return dp.get(n);
 
-            for (TreeNode left : leftTrees) {
-                for (TreeNode right : rightTrees) {
-                    TreeNode root = new TreeNode(0);
-                    root.left = left;
-                    root.right = right;
-                    ans.add(root);
-                }
+       List<TreeNode> ans=new ArrayList<>();
+
+       for(int i=1;i<n;i++){
+        List<TreeNode> left=allPossibleFBT(i);
+        List<TreeNode> right=allPossibleFBT(n-i-1);
+
+        for(TreeNode l:left){
+            for(TreeNode r:right){
+                TreeNode root=new TreeNode(0);
+                root.left=l;
+                root.right=r;
+                ans.add(root);
             }
         }
-
-        return ans;
+       }
+       dp.put(n,ans);
+       return ans;
     }
 }
