ec397eae8678a068bd573aa99444c1f485698b41
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-01 20:22:31 -0400

    Time: 0 ms (100%), Space: 40.6 MB (27.06%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -9,8 +9,15 @@ class Solution {
         // Bottom-up DP
         for (int i = 2; i <= n; i++) {
             int sum = 0;
+            //i=10
             for (int j = 1; j <= i; j++) {
+                //left how many are smaller than
+                //i ->i-1 ->3
                 int leftBST = dp[j - 1];
+                //right? (4)
+                //10-3 =>6
+                //right->4->5,6,7,8,9,10
+                //left->4->1,2,3
                 int rightBST = dp[i - j];
                 sum += leftBST * rightBST;
             }
