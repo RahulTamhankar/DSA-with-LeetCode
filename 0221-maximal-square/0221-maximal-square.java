bcfeb83ab4a8219b2c7eaacf6fb35f04cfcdc92d
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-02 23:07:38 -0400

    Time: 8 ms (69.59%), Space: 60 MB (77.62%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.java
+++ 0221-maximal-square/0221-maximal-square.java
@@ -1,6 +1,8 @@
 class Solution {
  
     public int maximalSquare(char[][] matrix) {
+                if (matrix == null || matrix.length == 0 || matrix[0].length == 0) return 0;
+
         int m = matrix.length;
         int n = matrix[0].length;
         int[][] dp = new int[m][n];
