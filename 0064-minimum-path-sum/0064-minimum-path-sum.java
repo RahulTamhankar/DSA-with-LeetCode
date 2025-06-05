69df5961f94d03ead0a56ad663ee98a447e32359
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-04 23:44:38 -0400

    Time: 3 ms (72.64%), Space: 47.5 MB (69.95%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,3 +1,35 @@
+// // Approach-1 (Recur + Memoization)
+// // T.C : O(m*n)
+// // S.C : O(m*n)
+// class Solution {
+//     private int MPS(int[][] grid, int i, int j, int m, int n, int[][] t) {
+//         if (i == m - 1 && j == n - 1)
+//             return t[i][j] = grid[i][j];
+//         if (t[i][j] != -1)
+//             return t[i][j];
+//         if (i == m - 1) { // we can only go right
+//             return t[i][j] = grid[i][j] + MPS(grid, i, j + 1, m, n, t);
+//         } else if (j == n - 1) { // we can only go down
+//             return t[i][j] = grid[i][j] + MPS(grid, i + 1, j, m, n, t);
+//         } else {
+//             return t[i][j] = grid[i][j] + Math.min(MPS(grid, i + 1, j, m, n, t), MPS(grid, i, j + 1, m, n, t));
+//         }
+//     }
+
+//     public int minPathSum(int[][] grid) {
+//         int m = grid.length;
+//         int n = grid[0].length;
+//         int[][] t = new int[m][n];
+//         for (int[] row : t) {
+//             Arrays.fill(row, -1);
+//         }
+//         return MPS(grid, 0, 0, m, n, t);
+//     }
+// }
+
+// Approach-2 (Bottom Up DP)
+// T.C : O(m*n)
+// S.C : O(m*n)
 class Solution {
     public int minPathSum(int[][] grid) {
         int m=grid.length;
