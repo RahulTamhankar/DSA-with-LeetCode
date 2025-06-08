abe05b37f3331c17a210f092941c16c11d20f1e0
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-08 00:32:51 -0400

    Time: 0 ms (100%), Space: 42.3 MB (15.46%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -4,9 +4,11 @@ public class Solution {
         if(n==0) return 0;
         int result=0;
         for(int i=0;i<32;i++){
-            result<<=1;
-            if((n&1)==1) result++;
-            n>>=1;
+            result<<=1; // Shift result left by 1 → Make room for the next bit.
+            if((n&1)==1) result++; // Put that bit into result’s last position (via result++)
+
+            n>>=1; // Shift n, right to move to the next bit.
+
         }
     return result;
     }
