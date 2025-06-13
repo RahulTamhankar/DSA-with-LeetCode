2ccab6cdee3b84975064fe0f3ec73b15e2ef160a
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-06-12 22:43:13 -0400

    Time: 26 ms (92.26%), Space: 92.2 MB (37.78%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.java
@@ -1,58 +1,42 @@
 class RandomizedSet {
-
-    Map<Integer, Integer> map = null;
-    List<Integer> arrayList = null;
-
-    Random rand = null;
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random random;
 
     /** Initialize your data structure here. */
     public RandomizedSet() {
+        list = new ArrayList<>();
         map = new HashMap<>();
-        arrayList = new ArrayList<>();
-        rand = new Random();
+        random = new Random();
     }
-
+    
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!map.containsKey(val)) {
-            map.put(val, arrayList.size());
-            arrayList.add(val);
-            return true;
-        } else{
+        if (map.containsKey(val)) {
             return false;
         }
-
+        list.add(val);
+        map.put(val, list.size() - 1);
+        return true;
     }
-
+    
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(map.containsKey(val)) {
-            // replace from last always
-
-
-            Integer lastNodeVal = arrayList.get(arrayList.size()-1);
-
-            if(lastNodeVal== val){
-                arrayList.remove(arrayList.size()-1);
-                map.remove(new Integer(val));
-                return true;
-            }
-            int indexOfVal = map.get(val);
-            arrayList.set(indexOfVal, lastNodeVal);
-            map.put(lastNodeVal, indexOfVal);
-            arrayList.remove(arrayList.size()-1);
-            map.remove(new Integer(val));
-            return true;
-        } else {
+        if (!map.containsKey(val)) {
             return false;
         }
+        int idx = map.get(val);
+        int lastElement = list.get(list.size() - 1);
+        list.set(idx, lastElement);
+        map.put(lastElement, idx);
+        list.remove(list.size() - 1);
+        map.remove(val);
+        return true;
     }
-
+    
     /** Get a random element from the set. */
     public int getRandom() {
-
-        int size = arrayList.size();
-        int index = rand.nextInt(size);
-        return arrayList.get(index);
+        int idx = random.nextInt(list.size());
+        return list.get(idx);
     }
 }
