22ab5433a91d51f38d9b5e03df52557efd782fe0
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-07-06 00:42:47 -0400

    Time: 6 ms (65.05%), Space: 45.7 MB (11.29%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -2,33 +2,31 @@ import java.util.*;
 
 class Solution {
     public int[] asteroidCollision(int[] asteroids) {
-        Stack<Integer> st = new Stack<>();
-        
-        for (int a : asteroids) {
-            while (!st.isEmpty() && a < 0 && st.peek() > 0) {
-                int sum = a + st.peek();
-                if (sum < 0) {
+        Stack<Integer> st=new Stack<>();
+
+        for(int a:asteroids){
+            while(!st.isEmpty() && a<0 && st.peek()>0){
+                int sum=a+st.peek();
+                if(sum<0){
                     st.pop();
-                } else if (sum > 0) {
-                    a = 0;
-                    break;
-                } else {
+                }else if(sum>0){
+                    a=0;
+                }else{
                     st.pop();
-                    a = 0;
+                    a=0;
                 }
             }
-            
-            if (a != 0)
+            if(a!=0){
                 st.push(a);
+            }
         }
-        
-        // Convert stack to int array
-        int[] result = new int[st.size()];
-        for (int i = st.size() - 1; i >= 0; i--) {
-            result[i] = st.pop();
+
+        int[] result=new int[st.size()];
+        for(int i=st.size()-1;i>=0;i--){
+            result[i]=st.pop();
         }
-        
         return result;
+    
     }
 }
 
