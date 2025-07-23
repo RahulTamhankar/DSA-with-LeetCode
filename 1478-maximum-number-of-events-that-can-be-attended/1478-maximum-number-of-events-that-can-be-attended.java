d9016cacf2bccd58917976f3b44c6ef11af5bc9f
Rahul Tamhankar <87760177+RahulTamhankar@users.noreply.github.com>
2025-07-22 22:08:19 -0400

    Time: 67 ms (32.87%), Space: 75.4 MB (86.1%) - LeetHub

--- 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -5,27 +5,24 @@ class Solution {
     public int maxEvents(int[][] events) {
         int n = events.length;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();//min heap
         Arrays.sort(events, (a, b) -> Integer.compare(a[0], b[0]));
         int day = events[0][0];
         int i = 0;
         int count = 0; // Result number of events attended
 
-        while(!pq.isEmpty() || i < n) {
+        while(!pq.isEmpty() || i < n) {// even if index is Out of Bound I need to see if there are elements in pq, hence 2 checks
 
-            if(pq.isEmpty()) {
-                day = events[i][0];
-            }
-
+              
             // Add all events that start on `day` to the min-heap
-            while(i < n && events[i][0] == day) {
-                pq.add(events[i][1]);
+            while(i < n && events[i][0] == day) { //if starting day of events[i] == starting day, then keep pushing them in pq
+                pq.add(events[i][1]);//just put event[i] ka 1st elements, as the 2nd one not needed
                 i++;
             }
 
             if(!pq.isEmpty()) {
-                pq.poll(); // Attend an event on this day
-                count++; // Counting the result
+                pq.poll(); // Attend an event on this day, use top ka events frpm pq
+                count++; // Counting the result, 1 event attended on this day. Makre sure before doing all this check if pq is not empty
             }
 
             day++;
